38dc5d882ebdcf8ccab48cd1442915460ac46a30
Kristijan Karanfiloski <kikaccc29@gmail.com>
2023-09-06 11:11:01 +0200

    Improved css for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SignupForm from "./signup form/SignupForm.jsx";
 import "./App.scss";
 
 import Modal from "./modal/Modal.jsx";
+import { ImgSvg } from "./svg/Img.jsx";
 
 function App() {
   const modal = useSelector((state) => state.modal.open);
@@ -13,10 +14,11 @@ function App() {
           <div className="newsLetter">
             <SignupForm />
             <div className="newsLetter__img">
-              <img
-                src="../public/illustration-sign-up-desktop.svg"
-                alt="signup img"
-              />
+              {/*<img*/}
+              {/*  src="../public/illustration-sign-up-desktop.svg"*/}
+              {/*  alt="signup img"*/}
+              {/*/>*/}
+              <ImgSvg />
             </div>
           </div>
           {/*<Todos />*/}
